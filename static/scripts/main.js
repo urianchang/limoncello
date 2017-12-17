2808c3660cd1f99aa04dfdbbcb308ff181fb7e5a
Urian <uchang@sightmachine.com>
2017-12-16 22:00:03 -0800

    Added sticky navbar and placeholder divs

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
+  var nav = $('.navbar');
+  var sticky = "sticky";
+  var header = $('.header').height();
 
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > header) {
+      nav.addClass(sticky);
+    } else {
+      nav.removeClass(sticky);
+    }
+  });
+
+  $('#navbar a').click(function(e) {
+    e.preventDefault();
+    $('html,body').scrollTo(this.hash, this.hash);
+  });
 });
